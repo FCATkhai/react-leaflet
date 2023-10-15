2406afb95959ac4860fc006d5ced6c1e00f1d8d5
FCATkhai <fcatkhai@gmail.com>
2023-10-15 16:42:25 +0700

    change MyMap

--- src/components/MyMap.jsx
+++ src/components/MyMap.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
-import {MapContainer, TileLayer, Marker, Popup, GeoJSON} from "react-leaflet";
-import { Icon } from 'leaflet';
+import {MapContainer, TileLayer, GeoJSON} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.webpack.css';
 import mapData from "../data/index";
@@ -10,13 +9,7 @@ import Context from '../store/Context';
 export default function MyMap_Test() {
     const position = [10.02, 105.46];
 
-    const customIcon = new Icon({
-        iconUrl: require("./../icons/location-icon.png"),
-        iconSize: [38, 38] // size of the icon
-    });
-
-
-    const [mapSelect, setMapSelect] = useContext(Context);
+    const [mapSelect, ] = useContext(Context);
     return (
         <MapContainer center={position} zoom={5} style={{height:"100vh", width: "100%"}} className='MapContainer'>
     <TileLayer
